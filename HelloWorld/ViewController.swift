6bb961a1b40903af1dcc5931a0d17ae749faf5d1
Deser7 <deser7@icloud.com>
2024-02-28 00:57:00 +0400

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Наташа Спиридонова on 27.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
